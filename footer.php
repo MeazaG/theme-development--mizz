a1bcc834b0f68e74ab00fe802a2401aaa09b5281
MeazaG <39376071+MeazaG@users.noreply.github.com>
2020-01-03 06:00:52 -0800

    Add files via upload

--- footer.php
+++ footer.php
@@ -6,35 +6,31 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Mizz
+ * @package Nature
  */
 
 ?>
 
 	</div><!-- #content -->
-   <footer class="site-footer">
-		<nav class="site-nav">
-			<?php 
-				$args=array(
-					'theme_location'=>'footer'
-					);
-			?>
-				<?php wp_nav_menu( $args); ?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mizz' ) ); ?>">
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nature' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'mizz' ), 'Meaza' );
+				printf( esc_html__( 'Proudly powered by %s', 'nature' ), 'WordPress' );
 				?>
 			</a>
-			</nav>
-		<p>Copy@ <?php the_date()?></p>
-	</footer>
-</div><!--container -->
-	
-		<?php wp_footer(); ?>
-	
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nature' ), 'nature', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<?php wp_footer(); ?>
 
 </body>
 </html>
