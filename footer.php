112359972b5119450a65fa0d886d494a4ef0f0aa
MeazaG <39376071+MeazaG@users.noreply.github.com>
2020-01-17 06:09:10 -0800

    Add files via upload

--- footer.php
+++ footer.php
@@ -14,18 +14,56 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nature' ) ); ?>">
+
+		<div id ="footer-sidebar"class="row secondary">
+			<div class="col-md-3" id="footer-sidebar1">
+
+		
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'nature' ), 'WordPress' );
+				if(is_active_sidebar('footer-sidebar-1')){
+					dynamic_sidebar('footer-sidebar-1');
+				}
+				
 				?>
-			</a>
+			
+		</div>
+		<div class ="col-md-3" id="footer-sidebar2">
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
+				if(is_active_sidebar('footer-sidebar-2')){
+					dynamic_sidebar('footer-sidebar-2');
+				}
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nature' ), 'nature', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
+				</div>
+				<div class="col-md-3" id="footer-sidebar3">
+					<?php
+                if(is_active_sidebar('footer-sidebar-3')){
+					dynamic_sidebar('footer-sidebar-3');
+				  }
+				  ?>
+				</div>
+				<div class="col-md-3" id="footer-sidebar4">
+					<?php
+                if(is_active_sidebar('footer-sidebar-4')){
+					dynamic_sidebar('footer-sidebar-4');
+				  }
+				  ?>
+				</div>
+
+				<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nature' ) ); ?>">
+            <?php
+				printf( esc_html__( 'Proudly powered by %s', 'nature' ), 'Miz' );
+				?>
+			</a>
+			<span class ="sep">|</span>
+			<?php
+			printf(esc_html_('Theme:1$s by %2$s.','nature'), 'nature','<a href="http://Underscores.me</a>');
+			?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
